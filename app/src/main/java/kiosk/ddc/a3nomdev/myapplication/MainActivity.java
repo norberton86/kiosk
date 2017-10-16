ae0839b881f47f5c7169c662a8144c9622da033b
3nomDev <3nomdev@3nomDevs-MacBook-Pro.local>
2017-10-16 15:23:14 -0400

    Scan done

--- app/src/main/java/kiosk/ddc/a3nomdev/myapplication/MainActivity.java
+++ app/src/main/java/kiosk/ddc/a3nomdev/myapplication/MainActivity.java
@@ -1,11 +1,17 @@
 package kiosk.ddc.a3nomdev.myapplication;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String codigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
         Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
         FontManager.markAsIconContainer(findViewById(R.id.mio), iconFont);
         FontManager.markAsIconContainer(findViewById(R.id.mio2), iconFont);
+
+        Button btn = (Button) findViewById(R.id.buttonScan);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
