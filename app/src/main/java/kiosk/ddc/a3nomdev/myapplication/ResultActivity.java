d29c751d7c1c1bf5e2a0ff588cca864af825453b
3nomDev <3nomdev@3nomDevs-MacBook-Pro.local>
2017-10-17 10:53:48 -0400

    Border

---
+++ app/src/main/java/kiosk/ddc/a3nomdev/myapplication/ResultActivity.java
@@ -0,0 +1,13 @@
+package kiosk.ddc.a3nomdev.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
