e3b359049ebfc13527ac4b591c0e165992d7f2da
3nomDev <3nomdev@3nomDevs-MacBook-Pro.local>
2017-10-19 10:28:08 -0400

    Icon Settings

--- app/src/main/java/kiosk/ddc/a3nomdev/myapplication/MainActivity.java
+++ app/src/main/java/kiosk/ddc/a3nomdev/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Typeface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 
@@ -13,8 +15,7 @@ import butterknife.OnClick;
 import kiosk.ddc.a3nomdev.myapplication.model.GitHub;
 import kiosk.ddc.a3nomdev.myapplication.service.ddcService;
 import rx.Observer;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
                                @Override
                                public void onError(Throwable e) {
+                                   Toast.makeText(MainActivity.this, "Error trying to login", Toast.LENGTH_SHORT).show();
                                }
 
                                @Override
@@ -68,5 +70,29 @@ public class MainActivity extends AppCompatActivity {
                                }
                            }
                 );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.settings)
+        {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
