18b7420f08adf178cbf9b341cf21712904dd1374
3nomdev <norberton86@gmail.com>
2017-12-15 15:07:55 -0500

    Keep Screen ON

--- app/src/main/java/kiosk/ddc/a3nomdev/myapplication/SplashActivity.java
+++ app/src/main/java/kiosk/ddc/a3nomdev/myapplication/SplashActivity.java
@@ -1,16 +1,10 @@
 package kiosk.ddc.a3nomdev.myapplication;
 
 import android.content.Intent;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import butterknife.OnClick;
 
 public class SplashActivity extends AppCompatActivity {
@@ -19,11 +13,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_splash);
         ButterKnife.inject(this);
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // to keep the screen ON
     }
 
     @OnClick(R.id.layoutMaster)
